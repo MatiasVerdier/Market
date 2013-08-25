e4a3861d4284230fbcaca36b2874755765a8d09e
Matias verdier <verdier.matias@gmail.com>
2013-08-25 20:19:51 -0300

    consultas sql insert usuarios, insert categorias, insert juegos

---
+++ src/baseDatos/insertCategorias.sql
@@ -0,0 +1,12 @@
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Accion');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Lucha');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Disparos');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Plataforma');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Musica');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Simulacion de combate');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Estrategia');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Simulacion de vida');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Arcade');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Puzle');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Aventura');
+INSERT INTO `market`.`categorias` (`nombre`) VALUES ('Rol');
